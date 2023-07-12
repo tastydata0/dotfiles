a85803952b47b419d7aea0a8e5c623d89bab7ae6
Alex <alex.zv.ev@gmail.com>
2023-07-12 09:23:03 +0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
 
 pip install meson
-pip install -r dwm-flexipatch/reqirements.txt
+pip install -r ./dwm-flexipatch/reqirements.txt
 
-sudo pacman -Sy `echo dwm-flexipatch/pacman_dependencies.txt`
-yay -Sy `echo dwm-flexipatch/yay_dependencies.txt`
+sudo pacman -Sy `cat dwm-flexipatch/pacman_dependencies.txt`
+yay -Sy `cat dwm-flexipatch/yay_dependencies.txt`
 
 cp -r .config/* ~/.config/
 cp -r dwm-flexipatch ~/
 
 cd ~/dwm-flexipatch && sudo make install
 
-cp dwm.desktop /usr/share/xsessions/
+sudo cp ./dwm.desktop /usr/share/xsessions/
 
 # Wallpapers
 mkdir ~/Pictures/Wallpapers
-cp -r Wallpapers/0 ~/Pictures/Wallpapers/
-cp -r Wallpapers/1 ~/Pictures/Wallpapers/
+cp -r ./Wallpapers/0 ~/Pictures/Wallpapers/
+cp -r ./Wallpapers/1 ~/Pictures/Wallpapers/
